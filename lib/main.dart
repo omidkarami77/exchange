4069eb389e5c68019ed4012923fa53b8a10b6013
omidkarami77 <omidtkd77@gmail.com>
2022-07-11 15:25:25 +0330

    fix bug project to translate

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:exchange/ui/ui_helper/ThemeSwitcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,23 +31,50 @@ class _MyMaterialAppState extends State<MyMaterialApp> {
     return Consumer<ThemeProvider>(
       builder: ((context, ThemeProvider, child) {
         return MaterialApp(
+          locale: Locale('fa', ''),
+          title: 'Localizations Sample App',
+          localizationsDelegates: const [
+            AppLocalizations.delegate, // Add this line
+
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', ''), // English, no country code
+            Locale('fa', ''), // Spanish, no country code
+          ],
           themeMode: ThemeProvider.themeMode,
           theme: MyThemes.lightTheme,
           darkTheme: MyThemes.darkTheme,
           debugShowCheckedModeBanner: false,
           home: Directionality(
             textDirection: TextDirection.ltr,
-            child: Scaffold(
-              appBar: AppBar(
-                actions: [ThemeSwitcher()],
-                title: Text('Karami Exchange'),
-                centerTitle: true,
-              ),
-              body: Container(),
-            ),
+            child: Body(),
           ),
         );
       }),
+    );
+  }
+}
+
+class Body extends StatefulWidget {
+  const Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [ThemeSwitcher()],
+        title: Text('Karami Exchange'),
+        centerTitle: true,
+      ),
+      body: Center(child: Text(AppLocalizations.of(context)!.helloWorld)),
     );
   }
 }
