469ac34e402f00e5c7b8c4dad8ef11b33015fb16
omidkarami77 <omidtkd77@gmail.com>
2022-07-11 19:05:05 +0330

    add bottom navigation to page

---
+++ lib/ui/ui_helper/bottom_nav.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  BottomNav({super.key, required this.controller});
+  PageController controller;
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      notchMargin: 5,
+      color: Colors.amber,
+      child: SizedBox(
+        height: 63,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        controller.animateToPage(0,
+                            duration: Duration(microseconds: 300),
+                            curve: Curves.easeInOut);
+                      },
+                      icon: Icon(Icons.home),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        controller.animateToPage(1,
+                            duration: Duration(microseconds: 300),
+                            curve: Curves.easeInOut);
+                      },
+                      icon: Icon(Icons.bar_chart),
+                    ),
+                  ]),
+              height: 50,
+              width: MediaQuery.of(context).size.width / 2 - 20,
+            ),
+            SizedBox(
+              height: 50,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        controller.animateToPage(2,
+                            duration: Duration(microseconds: 300),
+                            curve: Curves.easeInOut);
+                      },
+                      icon: Icon(Icons.verified_user),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        controller.animateToPage(3,
+                            duration: Duration(microseconds: 300),
+                            curve: Curves.easeInOut);
+                      },
+                      icon: Icon(Icons.bar_chart),
+                    ),
+                  ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
